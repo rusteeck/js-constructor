4f57b6c4a3af4ebc77ebdb35eb405ce3d1e0f0cb
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-12-20 22:58:53 +0700

    - base js templator

---
+++ index.js
@@ -0,0 +1,65 @@
+const model =[
+    {
+        type: 'title',
+        value: 'Hello from js'
+    },
+
+    {
+        type: 'text',
+        value: 'Some text'
+    },
+
+    {
+        type: 'columns',
+        value: [
+            '1111111','222222','333333333'
+        ]
+    }
+]
+
+const $site = document.querySelector('#site')
+
+model.forEach(block => {
+    let html = ''
+    if (block.type === 'title'){
+        html = title(block)
+    } else if (block.type === 'text'){
+      html =  title(block)
+    } else if (block.type === 'columns'){
+        html = columns(block)
+    }
+
+$site.insertAdjacentHTML('beforeend', html)
+
+})
+
+function title(block){
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+        </div>
+`
+}
+
+function text(block){
+    return `
+        <div class="row">
+            <div class="col-sm">
+                <p>${block.value}</p>
+            </div>
+        </div>
+`
+}
+
+function columns(block){
+    const column = block.value.map(col => `<div class="col-sm">${col}</div>`)
+    return `
+        <div class="row">
+            ${column.join('')}            
+        
+    )}
+        </div>
+        `
+}
