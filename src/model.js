b0f39ed1fe9507d03c4f968f261fd708e4f966ab
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-12-20 23:29:02 +0700

    - code struct

---
+++ src/model.js
@@ -0,0 +1,9 @@
+export const model =[
+    {type: 'title', value: 'Hello from js'},
+
+    {type: 'text', value: 'Some text'},
+
+    {type: 'columns', value: ['1111111','222222','333333333']},
+
+    {type: 'image', value: './assets/image.png'}
+]
--- src/templates.js
+++ src/templates.js
@@ -1,31 +1,3 @@
-const model =[
-    {type: 'title', value: 'Hello from js'},
-
-    {type: 'text', value: 'Some text'},
-
-    {type: 'columns', value: ['1111111','222222','333333333']},
-
-    {type: 'image', value: './assets/image.png'}
-]
-
-const $site = document.querySelector('#site')
-
-model.forEach(block => {
-    let html = ''
-    if (block.type === 'title'){
-        html = title(block)
-    } else if (block.type === 'text'){
-      html =  title(block)
-    } else if (block.type === 'columns'){
-        html = columns(block)
-    } else if (block.type === 'image'){
-        html = image(block)
-    }
-
-$site.insertAdjacentHTML('beforeend', html)
-
-})
-
 function title(block){
     return `
         <div class="row">
@@ -64,3 +36,5 @@ function image(block){
         </div>
         `
 }
+
+export {title, text,columns, image}
